1fd3c81972e25aaf91290bca09a8fb375687ca2a
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2021-09-26 23:37:12 +0600

    little change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="header">
             <h1>Business Masterminds</h1>
             <p>These people have one of the finest minds filled with brilliant ideas in the world right now.</p>
-            <h3>They have in total net worth of 748.8 Billion Dollars</h3>
+            <h2>They have in total net worth of 748.8 Billion Dollars</h2>
         </div>
     );
 };
